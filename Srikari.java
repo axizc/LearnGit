d814d04b8467c45351aae3761bde0763221f4880
SrikariTanikella <145238991+SrikariTanikella@users.noreply.github.com>
2024-07-08 22:32:51 -0700

    Srikari class
    Created birthday method

---
+++ Srikari.java
@@ -0,0 +1,20 @@
+public class Srikari public class Srikari {
+    private int birthdayage;
+
+    private String birthdaymonth;
+
+    public Srikari ( int age , String month){
+        birthdayage = age;
+        birthdaymonth = month;
+    }
+
+    public Srikari ( int age ) {
+        birthdayage = age;
+        birthdaymonth = "unknown";
+    }
+
+    public int getSrikariAmeya(){
+        return birthdayage;
+    }
+}{
+}
